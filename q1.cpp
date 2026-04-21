8690c8d09c4a064a19691c77e7fc203dfffa1288
nkirimi <131856861+nkirimi@users.noreply.github.com>
2026-04-21 23:37:49 +0300

    Add files via upload

---
+++ q1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int days;
+    cout << "Enter number of days: ";
+    cin >> days;
+
+    // 1 day = 24 hrs * 60 mins * 60 secs
+    long long seconds = days * 24 * 60 * 60;
+
+    cout << days << " day(s) = " << seconds << " seconds" << endl;
+    return 0;
+}
